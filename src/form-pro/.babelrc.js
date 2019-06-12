19d6ba4437867cba2827bec967eb2e90d282ad82
zuiidea <zuiiidea@gmail.com>
2019-06-12 16:26:04 +0800

    🔧  Using babel package, tsc generate declaration file.

---
+++ src/form-pro/.babelrc.js
@@ -0,0 +1,19 @@
+module.exports = function(api) {
+  const env = api.cache(() => process.env.TARGET_ENV)
+  return {
+    presets: [
+      [
+        '@babel/env',
+        {
+          modules: env === 'esm' ? false : 'auto',
+        },
+      ],
+
+      ['react-app', { flow: false, typescript: true }],
+    ],
+
+    plugins: [['babel-plugin-import', { libraryName: 'antd', style: true }]],
+
+    ignore: ['src/__tests__/', 'src/__stories__/'],
+  }
+}
